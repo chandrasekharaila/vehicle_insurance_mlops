436cb553e26c325dd8d04ea14e9360e9089b002f
Chandra Sekhar Aila <tarunaila007@gmail.com>
2026-07-18 14:01:46 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Aila Chandra Sekhar",
+    author_email="chandraaila5@gmail.com",
+    packages=find_packages()
+)
